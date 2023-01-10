9f2dea80a7afc92ce5cc9dfade12dfb802673988
jamiegledhill <32931760+jamiegledhill@users.noreply.github.com>
2023-01-10 11:56:06 +0000

    Multiple Waves

--- js/sketch.js
+++ js/sketch.js
@@ -7,17 +7,30 @@ let amplitude;
 let frequency;
 let maxAngle;
 function setup() {
-    amplitude = windowHeight / 8;
-    frequency = windowWidth / 8;
+    amplitude = windowHeight / 4;
+    frequency = windowWidth / 4;
     maxAngle = (windowWidth / frequency) * TWO_PI;
     createCanvas(windowWidth, windowHeight);
 }
 function draw() {
     background(0);
     noStroke();
-    fill(255, 255, 255);
     while (theta < maxAngle + offset) {
+        // sine wave 3
+        y = sin(theta - 0.4) * amplitude;
+        fill(50, 0, 0);
+        ellipse(x, y + height / 2, 16);
+        // sine wave 2
+        y = sin(theta - 0.2) * amplitude;
+        fill(150, 0, 0);
+        ellipse(x, y + height / 2, 16);
+        // sine wave 1
         y = sin(theta) * amplitude;
+        fill(255, 0, 0);
+        ellipse(x, y + height / 2, 16);
+        // cosine wave
+        y = cos(theta) * amplitude;
+        fill(0, 255, 0);
         ellipse(x, y + height / 2, 16);
         theta += 0.2;
         x = ((theta - offset) / maxAngle) * windowWidth;
