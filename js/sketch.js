6ae8dd1fe3a0e70bfbeea713cfb85f2364a3a5f0
jamiegledhill <32931760+jamiegledhill@users.noreply.github.com>
2023-01-10 11:46:24 +0000

    Animated Sine Wave

--- js/sketch.js
+++ js/sketch.js
@@ -1,17 +1,27 @@
+let x = 0;
+let y = 0;
+let theta = 0;
+let inc = 0.05; // the amount to increment the offset each frame
+let offset = 0; // the amount offset from beginning of wave
+let amplitude;
+let frequency;
+let maxAngle;
 function setup() {
+    amplitude = windowHeight / 8;
+    frequency = windowWidth / 8;
+    maxAngle = (windowWidth / frequency) * TWO_PI;
     createCanvas(windowWidth, windowHeight);
-    let x = 0;
-    let y = 0;
-    let theta = 0;
-    let amplitude = windowHeight/8;
-    let frequency = windowWidth/8;
-    let maxAngle = (windowWidth/frequency) * TWO_PI;
-    while(theta < maxAngle){
-        y = sin(theta) * amplitude;
-        ellipse(x, y + height/2, 16);
-        theta += 0.2;
-        x = (theta/maxAngle) * windowWidth;
-    }
 }
 function draw() {
+    background(0);
+    noStroke();
+    fill(255, 255, 255);
+    while (theta < maxAngle + offset) {
+        y = sin(theta) * amplitude;
+        ellipse(x, y + height / 2, 16);
+        theta += 0.2;
+        x = ((theta - offset) / maxAngle) * windowWidth;
+    }
+    offset += inc;
+    theta = offset;
 }
